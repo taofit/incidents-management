2ef3795708a0865e94525079076421fc305839cc
Tao <tao.sun@zymego.com>
2022-11-01 18:05:13 +0100

    add styles

--- app/src/App.js
+++ app/src/App.js
@@ -19,31 +19,30 @@ const App = () => {
 
   return (
     <div className="bg-primary h-screen overflow-y-auto min-h-full px-4">
-      <Text color="primary" className="py-8">
-        Hello, World!
-      </Text>
-      <div className="text-white bg-secondary rounded-xl h-auto p-5">
-        <Text color="default">Let's get started.</Text>
+      <div className="text-white rounded-xl h-auto p-5">
+        <Text className="title">All incidents</Text>
       </div>
       {loading && <p>loading...</p>}
       {error && (<div>{`There is a problem fetching the post data- ${error}`}</div>)}
       <table>
+      <caption color="default">All incidents</caption>
       <thead>
         <tr><td>ID</td><td>PROBLEM</td><td>CINEMA</td><td>ASSIGNEE</td><td>LAST ACTIVITY</td><td>CREATED</td><td>STATUS</td></tr>
       </thead> 
       <tbody>
         {data && 
-        Object.keys(data).map((key) => (
-          <tr key={data[key]._id}>
-            <td>{data[key]._id}</td> 
+        Object.keys(data).map((key) => {
+          const status = data[key].warning.status === 'open' ? 'status-open' : 'status-closed';
+         return <tr key={data[key]._id}>
+            <td className="id">{data[key]._id}</td> 
             <td>{data[key].warning.errorDescription}</td> 
             <td>{data[key].cinema.name}-{data[key].cinema.id}</td> 
             <td>{data[key].assignee?.name}</td>
             <td>{data[key].updatedAt}</td>
             <td>{data[key].createdAt}</td>
-            <td>{data[key].warning.status}</td>
+            <td className={status}><span>{data[key].warning.status}</span></td>
           </tr>
-        ))} 
+        })} 
 
       </tbody>
       </table>
